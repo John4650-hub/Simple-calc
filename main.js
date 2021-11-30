782fab37b932e2714a81526c560c38050e7babb4
JamyJones <93288699+JamyJones@users.noreply.github.com>
2021-11-30 13:35:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// alert('This calculator may not appear as it should if your runnning it in an old browser version');
-// Numbers and keys
+
+// Numbers and keys block
 const num1 = document.getElementById('num1').value;
 const num2 = document.getElementById('num2').value;
 const num3 = document.getElementById('num3').value;
@@ -11,7 +11,7 @@ const num8 = document.getElementById('num8').value;
 const num9 = document.getElementById('num9').value;
 const num0 = document.getElementById('num0').value;
 
-// Mathematical symbols and key
+// Mathematical symbols and key block
 const equals = document.getElementById('equals').value;
 const plus = document.getElementById('plus').value;
 const minus = document.getElementById('minus').value;
@@ -21,8 +21,9 @@ const curlsbtn = document.getElementById('curlsbtn').value;
 const modulus = document.getElementById('perc').value;
 const point = document.getElementById('point').value;
 
+let clearBtn  = document.getElementById('cbtn');
 
-// Input & Output for displaying numbers
+// Input & Output block
 var entry = document.getElementById('entry');
 var output = document.getElementById('output');
 
@@ -35,12 +36,11 @@ function get(element)
 }
 
 let usr = '';
-
+// logic block
 function getAns(element = equals)
 {
   usr += entry.value
   entry.value = '';
-  console.log(usr);
   let a, b;
   let ans = usr.trim()
   let pls = ans.indexOf('+');
@@ -49,7 +49,7 @@ function getAns(element = equals)
     b = Number(ans.slice(pls + 1, ans.length));
     let sum = a + b;
     output.value += sum;
-    
+
   }
 
   let sbtrct = ans.indexOf('-');
@@ -58,8 +58,43 @@ function getAns(element = equals)
     b = Number(ans.slice(sbtrct + 1, ans.length));
     let diff = a - b;
     output.value += diff;
+  }
+  let dvd = ans.indexOf('÷');
+  if (dvd != -1) {
+    a = Number(ans.slice(0, dvd));
+    b = Number(ans.slice(dvd + 1, ans.length));
+    let divsion = a / b;
+    output.value += divsion;
 
   }
+
+  let remda = ans.indexOf('%');
+  if (remda != -1) {
+    a = Number(ans.slice(0, remda));
+    b = Number(ans.slice(remda + 1, ans.length));
+    let remain = a % b;
+    output.value += remain;
+  }
+
+  let pdt = ans.indexOf('*');
+  if (pdt != -1) {
+    a = Number(ans.slice(0, pdt));
+    b = Number(ans.slice(pdt + 1, ans.length));
+    let product = a * b;
+    output.value += product;
+  }
+  
   usr = '';
 
 }
+
+function clearing(element = clearBtn){
+  entry.value = '';
+  output.value = '';
+}
+function noSupport(){
+ alert("key logic still in development") 
+}
+alert('I used the grid layout module that is only supported in all modern browsers, so this calculator might appear different if you run it in an outdated browser')
+
+alert("This calculator only does a single operation at a time so you can't do more than one operation.The C button can be used to clear out any data.")
