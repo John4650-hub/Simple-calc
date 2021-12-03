3f3440fd29d36397245b830b1be8fbda1b23d155
JohnDelvin <84484398+John4650-hub@users.noreply.github.com>
2021-12-03 19:38:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // Numbers and keys block
 const num1 = document.getElementById('num1').value;
 const num2 = document.getElementById('num2').value;
@@ -21,17 +20,15 @@ const curlsbtn = document.getElementById('curlsbtn').value;
 const modulus = document.getElementById('perc').value;
 const point = document.getElementById('point').value;
 
-let clearBtn  = document.getElementById('cbtn');
+let clearBtn = document.getElementById('cbtn');
 
 // Input & Output block
 var entry = document.getElementById('entry');
-var output = document.getElementById('output');
-
 
 function get(element)
 {
   entry.value += element;
-  output.value = '';
+  
 
 }
 
@@ -41,60 +38,17 @@ function getAns(element = equals)
 {
   usr += entry.value
   entry.value = '';
-  let a, b;
-  let ans = usr.trim()
-  let pls = ans.indexOf('+');
-  if (pls != -1) {
-    a = Number(ans.slice(0, pls));
-    b = Number(ans.slice(pls + 1, ans.length));
-    let sum = a + b;
-    output.value += sum;
-
-  }
-
-  let sbtrct = ans.indexOf('-');
-  if (sbtrct != -1) {
-    a = Number(ans.slice(0, sbtrct));
-    b = Number(ans.slice(sbtrct + 1, ans.length));
-    let diff = a - b;
-    output.value += diff;
-  }
-  let dvd = ans.indexOf('÷');
-  if (dvd != -1) {
-    a = Number(ans.slice(0, dvd));
-    b = Number(ans.slice(dvd + 1, ans.length));
-    let divsion = a / b;
-    output.value += divsion;
-
-  }
-
-  let remda = ans.indexOf('%');
-  if (remda != -1) {
-    a = Number(ans.slice(0, remda));
-    b = Number(ans.slice(remda + 1, ans.length));
-    let remain = a % b;
-    output.value += remain;
-  }
-
-  let pdt = ans.indexOf('*');
-  if (pdt != -1) {
-    a = Number(ans.slice(0, pdt));
-    b = Number(ans.slice(pdt + 1, ans.length));
-    let product = a * b;
-    output.value += product;
-  }
+  
+    let ans = eval(usr)
+    entry.value += ans;
   
   usr = '';
 
 }
 
-function clearing(element = clearBtn){
+function clearing(element = clearBtn) {
   entry.value = '';
-  output.value = '';
 }
-function noSupport(){
- alert("key logic still in development") 
+function negativate(){
+  entry.value[0] = '-';
 }
-alert('I used the grid layout module that is only supported in all modern browsers, so this calculator might appear different if you run it in an outdated browser')
-
-alert("This calculator only does a single operation at a time so you can't do more than one operation.The C button can be used to clear out any data.")
